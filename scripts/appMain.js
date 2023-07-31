263e8637bf65f8d91eb6f728a2285524d12c43ce
Andrei Petrusevich <danunafi123@gmail.com>
2023-07-31 15:59:55 +0300

    1 render media to new css, 2 create cards using js to optimize and get from an array, 3 create new css to work with visual responses

--- scripts/appMain.js
+++ scripts/appMain.js
@@ -1,30 +1,62 @@
-// functions
+// ----------- functions ----------
+
+// возвращает созданный эллемент с присвоенным классом
 const createElementAndClass = (tagName, className) => {
-  let element = document.createElement(tagName);
+  const element = document.createElement(tagName);
   element.classList.add(className);
 
   return element;
 };
 
-const createNewDiv = () => {
-  const newDiv = createElementAndClass("div", "card-about-us");
-  const newIcon = createElementAndClass("i", "fa-solid");
-  newIcon.classList.add("fa-city");
+// возвращает карточку "about-us" с уже готовым контентом и классами
+const createNewCard = (iconClass1, iconClass2, link, textP) => {
+  const newA = createElementAndClass("a", "card-about-us");
+  newAttribute = document.createAttribute("href");
+  newAttribute.value = link;
+  newA.setAttributeNode(newAttribute);
+  const newIcon = createElementAndClass("i", "icon");
+  newIcon.classList.add(iconClass1, iconClass2);
   const newP = document.createElement("p");
-  newP.textContent = "СТРОИТЕЛЬСТВО ОФИСНЫХ ЗДАНИЙ";
+  newP.textContent = textP;
 
-  newDiv.appendChild(newIcon);
-  newDiv.appendChild(newP);
-  return newDiv;
+  newA.appendChild(newIcon);
+  newA.appendChild(newP);
+  return newA;
 };
 
-// works block
+// якобы оправляет запрос на сервер для получения объектов
+// возвращает массив объектов для наполнения карточек
+const makeArray = (lengthArray) => {
+  let array = [];
 
+  for (let i = 0; i < lengthArray; i++) {
+    // якобы получение объекта с сервера
+    let someObjFromServer = {
+      iconClass1: "fa-solid",
+      iconClass2: "fa-city",
+      link: "#",
+      textP: "СТРОИТЕЛЬСТВО ОФИСНЫХ ЗДАНИЙ",
+    };
+    array[i] = someObjFromServer;
+  }
+  return array;
+};
+
+// создание полей по массиву
+const makerCards = (square, array) => {
+  for (let i = 0; i < array.length; i++) {
+    let obj = array[i];
+    square.appendChild(createNewCard(obj.iconClass1, obj.iconClass2, obj.link, obj.textP));
+  }
+};
+
+// ---------- works block ----------
+// получаем поля которые нужно заполнить карточками
 const big = document.querySelector(".big");
 const small = document.querySelector(".small");
-
-for (let i = 0; i < 3; i++) {
-  small.appendChild(createNewDiv());
-  big.appendChild(createNewDiv());
-  big.appendChild(createNewDiv());
-}
+// создаем массивы объектов с заданной длинной
+const arrObjsForSmall = makeArray(3);
+const arrObjsForBog = makeArray(6);
+// создаем карточки по обьектам в нужных полях
+makerCards(small, arrObjsForSmall);
+makerCards(big, arrObjsForBog);
